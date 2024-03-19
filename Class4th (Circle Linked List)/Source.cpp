380abb4dc2b8993d4e29ffa7e083c5962b288370
lim7635 <ju7635@naver.com>
2024-03-19 21:43:30 +0900

    2024.03.19 원형 큐

---
+++ Class4th (Circle Linked List)/Source.cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+class CircleLinkedList
+{
+private:
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+
+	Node* head;
+
+	int size;
+
+public:
+	CircleLinkedList()
+	{
+		head = nullptr;
+
+		size = 0;
+	}
+
+	void PushFront(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = nullptr;
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->next = head;
+		}
+		else
+		{
+			newNode->next = head->next;
+			head->next = newNode;
+		}
+		size++;
+	}
+
+	void PushBack(T data)
+	{
+		Node* newNode = new Node;
+		newNode->data = data;
+		newNode->next = nullptr;
+		if (head == nullptr)
+		{
+			head = newNode;
+			newNode->next = head;
+		}
+		else
+		{
+			head->next = newNode;
+			newNode->next = head->next;
+			head = newNode;
+		}
+		size++;
+	}
+
+	void PopFront()
+	{
+		if (size == 0)
+		{
+			cout << "List is Empty" << endl;
+			return;
+		}
+
+		Node* deleteNode = nullptr;
+		deleteNode = head->next;
+
+		if (size == 1)
+		{
+			head = nullptr;
+		}
+		else
+		{
+			head->next = deleteNode->next;
+		}
+		delete deleteNode;
+		size--;
+	}
+
+	void PopBack()
+	{
+		if (size == 0)
+		{
+			cout << "List is Empty" << endl;
+			return;
+		}
+
+		Node* deleteNode = head;
+		Node* currentNode = head;
+		if (size == 1)
+		{
+			head = nullptr;
+		}
+		else
+		{
+			for (int i = 0; i < size - 1; i++)
+			{
+				currentNode = currentNode->next;
+			}
+			currentNode->next = head->next;
+			head = currentNode;
+		}
+		delete deleteNode;
+		size--;
+	}
+
+	~CircleLinkedList()
+	{
+		while (size != 0)
+		{
+			PopBack();
+		}
+	}
+};
+
+int main()
+{
+	CircleLinkedList<int> circlelinkedlist;
+
+	circlelinkedlist.PushBack(100);
+	circlelinkedlist.PushFront(99);
+	circlelinkedlist.PushFront(97);
+	circlelinkedlist.PushBack(95);
+
+	circlelinkedlist.PopBack();
+
+	return 0;
+}
---
+++ Class5th (Linear Queue)/Source.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+using namespace std;
+
+#define SIZE 5
+
+template <typename T>
+class LinearQueue
+{
+private:
+	T data;
+	T array[SIZE];
+
+	int size;
+	int rear;
+	int front;
+
+public:
+	LinearQueue()
+	{
+		size = 0;
+		rear = 0;
+		front = 0;
+	}
+
+	void Push(T data)
+	{
+		if (IsFull())
+		{
+			cout << "Queue is Full" << endl;
+		}
+		else
+		{
+			array[rear] = data; // array[rear++] = data와 동일(후위 증감)
+			rear++;
+			size++;
+		}
+	}
+
+	void Pop()
+	{
+		if (Empty())
+		{
+			cout << "Queue is Empty" << endl;
+		}
+		else
+		{
+			array[front++] = NULL; // (array[front] = NULL -> front++)와 동일(후위 증감)
+			size--;
+		}
+	}
+
+	bool IsFull()
+	{
+		if (rear == SIZE && front == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool Empty()
+	{
+		if (rear == front)
+		{
+			return true;
+		}
+		return false;
+	}
+
+	// 복사 비용을 줄이기 위해 '참조자(&)' 사용
+	int& Size()
+	{
+		return size;
+	}
+
+	T& Front()
+	{
+		if (rear == front)
+		{
+			exit(1);
+		}
+		else
+		{
+			return array[front];
+		}
+	}
+};
+
+int main()
+{
+#pragma region 선형 큐
+	// 배열의 공간에 들어간 데이터가 고정되어 데이터를 빼내더라도 초기화하지 않으며
+	// 원래 데이터가 있던 배열의 자리에 더 이상 다른 값이 들어올 수 없는 Queue입니다.
+
+	LinearQueue<int> linearqueue;
+
+	linearqueue.Push(10);
+	linearqueue.Push(20);
+	linearqueue.Push(30);
+	linearqueue.Push(40);
+	linearqueue.Push(50);
+
+	while (linearqueue.Empty() == false)
+	{
+		cout << linearqueue.Front() << endl;
+		linearqueue.Pop();
+	}
+
+#pragma endregion
+
+	return 0;
+}
